5b5fe65fb3bd1293994a5c49ec9a93ae4334e394
Hernanrodriguezcatalano <hernanrodriguezcatalano@gmail.com>
2018-03-12 12:49:06 -0300

    Update QueueContiguoTest.cpp

--- QueueContiguoTest.cpp
+++ QueueContiguoTest.cpp
@@ -11,21 +11,18 @@ static queue Tqueue;
 static int a = 0;
 static int c;
 int main() {
+	
 	cout << "Seleccione la funcion a testear:" << endl << "1.Enqueue" << endl << "2.Dequeue" << endl << "3.First" << endl;
 	cin >> c;
 	assert(c > 0 && c < 4);
 	switch (c)
 	{
 	case 1:
-		while (a < M)
-		{
+		for (size_t i = 0; i < M; i++) {
+
 			enQueue(Tqueue, rand() % 100);
-			enQueue(Tqueue, rand() % 100);
-			cout << " valor "<<Tqueue.b[a] << " Front "<<Tqueue.front << " Rear "<<Tqueue.rear <<" Size "<< Tqueue.size_t;
-			deQueue(Tqueue);
-			++a;
+			cout << "Valor:" << Tqueue.b[i] << " Front:" << Tqueue.front << " Rear:" << Tqueue.rear << " Size:" << Tqueue.size_t << endl;
 		}
-		cin >> a;
 		cout << "Intentando encolar un elemento a la Queue llena...";
 		Sleep(3000);
 		enQueue(Tqueue, rand() % 100);
